95e7403a7f45d987e261d64a9cc67ef04981b3ee
runrun16 <yamapink49@gmail.com>
2019-02-17 20:34:04 -0500

    pylint outcome

--- src/outcome.py
+++ src/outcome.py
@@ -1,25 +1,30 @@
 class Outcome:
-    """A single outcome that could be bet on"""
+    """A single outcome that could be bet on
+
+    Attributes:
+        name (int): outcome name
+        odss (int): odds: 1
+    """
 
     def __init__(self, name, odds):
         self.name = name
         self.odds = odds
 
-    def winAmount(self, amount):
+    def win_amount(self, amount):
         """how much win if put amount of bet on this outcome
 
         Args:
             amount (int): amount put on the bet
-       
+
         Returns:
             int: how many money win
 
        """
-            
+
         return amount * self.odds
 
     def __eq__(self, other):
-        """atrribute equality"""       
+        """atrribute equality"""
         return self.name == other.name
 
     def __ne__(self, other):
